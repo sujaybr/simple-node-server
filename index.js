7b0d25709be572490ae2fc22a05332f184de1838
Sujay B R <sujaybr2729@gmail.com>
2025-08-28 12:40:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 
-import Mixpanel from "mixpanel";
 import { PostHog } from 'posthog-node'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const mp = Mixpanel.init(process.env["MIX_PANEL_KEY"]);
 const ph = new PostHog(
   process.env['POST_HOG_KEY'],
   { host: 'https://us.i.posthog.com' }
 );
+const logs = [];
 
 app.use(cors());
 app.use(helmet());
@@ -20,20 +19,6 @@ app.use(helmet());
 app.get("/", (req, res) => {
   console.log("GET / - Health check");
   res.send("OK");
-});
-
-app.get("/mp/:project/:tag/:extra?", (req, res) => {
-  const { project, tag, extra } = req.params;
-
-  mp.track(tag, {
-    distinct_id: 'default-user',
-    project: project,
-    tag: tag,
-    extra: extra || null,
-  });
-
-  console.log(`Mixpanel event, tag: '${tag}', project: '${project}', extra: '${extra || "N/A"}'`);
-  res.send("DONE");
 });
 
 app.get("/ph/:project/:tag/:extra?", (req, res) => {
@@ -50,12 +35,23 @@ app.get("/ph/:project/:tag/:extra?", (req, res) => {
       }
     });
 
+    logs.push({
+      timestamp: new Date().toISOString(),
+      project,
+      tag,
+      extra: extra || null
+    });
+
     console.log(`PostHog event, tag: '${tag}', project: '${project}', extra: '${extra || "N/A"}'`);
     res.send("DONE");
   } catch (error) {
     console.error(`Error tracking event in PostHog: ${error.message}`);
     res.status(500).json({ success: false, error: error.message });
   }
+});
+
+app.get("/logs", (req, res) => {
+  res.json(logs);
 });
 
 app.listen(PORT, () => {
